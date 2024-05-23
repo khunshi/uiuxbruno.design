dc3ebbc515cd8773c049bae5cf3b8f3e88f8faf1
Bruno Santos <brunoheroesevolved@gmail.com>
2024-05-23 13:02:18 -0300

    basicamente pronto

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
-window.addEventListener('scroll', function() {
-    console.log('escutou')
-    document.querySelector('.arrow-down').classList.remove('animation-fade');
-    document.querySelector('.arrow-down').classList.add('hidden');
-  });
+window.addEventListener('scroll', function () {
+  document.querySelector('.arrow-down').classList.remove('animation-fade');
+  document.querySelector('.arrow-down').classList.add('hidden');
+}
+);
+
+const copiar = (botao) => {
+  const email = "uiuxbruno.design@gmail.com";
+  const number = "19988705203";
+
+  if (botao.id == "copyEmail") {
+
+    navigator.clipboard.writeText(email);
+
+    botao.innerHTML = "Texto copiado!";
+
+    setTimeout(() => {
+      botao.innerHTML = "Copiar para área de transfêrencia";
+    }, 1500);
+
+  }
+  else if (botao.id == "copyNumber") {
+
+    navigator.clipboard.writeText(number);
+
+    botao.innerHTML = "Texto copiado!"
+
+    setTimeout(() => {
+      botao.innerHTML = "Copiar para área de transfêrencia";
+    }, 1500);
+
+  }
+}
